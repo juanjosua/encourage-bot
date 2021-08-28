232432e5c4d026e34bec66c3aaeb369756337a77
Juan Josua <47451026+juanjosua@users.noreply.github.com>
2021-08-28 08:59:33 +0000

    add a list of sad words and encouragements, add new function to the bot to recognize sad words, and send random encouragements

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import discord
 import os
 import requests
 import json
+import random
 
 client = discord.Client()
+
+sad_words = ['sad', 'depressed', 'unhappy', 'angry', 'miserable', 'depressing']
+
+starter_encouragements = [
+  'Cheer up!',
+  'Hang in there.',
+  'You are a great person / bot!'
+]
 
 def get_quote():
   response = requests.get('https://zenquotes.io/api/random')
@@ -22,12 +31,17 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
+  msg = message.content
+
+  if msg.startswith('$hello'):
     await message.channel.send('Hello!')
 
-  if message.content.startswith('$inspire'):
+  if msg.startswith('$inspire'):
     quote = get_quote()
     await message.channel.send(quote)
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
 
 
 client.run(os.getenv('TOKEN'))
