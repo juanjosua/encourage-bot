a52d051742a051a32bcf48caadb5dcdb0b2b0bee
Juan Josua <47451026+juanjosua@users.noreply.github.com>
2021-08-29 09:30:36 +0000

    user able to turn off the respond, delete encouragement message

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ starter_encouragements = [
   'You are a great person / bot!'
 ]
 
+if 'responding' not in db.keys():
+  db['responding'] = True
+
 def get_quote():
   response = requests.get('https://zenquotes.io/api/random')
   json_data = json.loads(response.text)
@@ -57,12 +60,13 @@ async def on_message(message):
     quote = get_quote()
     await message.channel.send(quote)
 
-  options = starter_encouragements
-  if 'encouragements' in db.keys():
-    options = options + list(db['encouragements'])
+  if db['responding']:
+    options = starter_encouragements
+    if 'encouragements' in db.keys():
+      options = options + list(db['encouragements'])
 
-  if any(word in msg for word in sad_words):
-    await message.channel.send(random.choice(options))
+    if any(word in msg for word in sad_words):
+      await message.channel.send(random.choice(options))
 
   if msg.startswith('$new'):
     encouraging_message = msg.split('$new ', 1)[1]
@@ -74,8 +78,25 @@ async def on_message(message):
     if 'encouragements' in db.keys():
       index = int(msg.split('$del', 1)[1])
       delete_encouragements(index)
-      encouragements = db['encouragements']
-      await message.channel.send(encouragements)
+      encouragements = list(db['encouragements'])
+    await message.channel.send('Encouragement message deleted.')
+    await message.channel.send(encouragements)
+
+  if msg.startswith('$list'):
+    encouragements = []
+    if 'encouragements' in db.keys():
+      encouragements = list(db['encouragements'])
+    await message.channel.send(encouragements)
+
+  if msg.startswith('$responding'):
+    value = msg.split('$responding ', 1)[1]
+
+    if value.lower() == 'true':
+      db['responding'] = True
+      await message.channel.send('Responding is on.')
+    else:
+      db['responding'] = False
+      await message.channel.send('Responding is off.')
 
 
 client.run(os.getenv('TOKEN'))
