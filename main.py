3e5f29e88dc87a721e0a4fbf5dae907fe31ed54a
Juan Josua <47451026+juanjosua@users.noreply.github.com>
2021-08-29 09:18:51 +0000

    User able to add new encouragement messages

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import requests
 import json
 import random
+from replit import db
 
 client = discord.Client()
 
@@ -19,6 +20,22 @@ def get_quote():
   json_data = json.loads(response.text)
   quote = json_data[0]['q'] + ' -' + json_data[0]['a']
   return quote
+
+
+def update_encouragements(encouraging_message):
+  if 'encouragements' in db.keys():
+    encouragements = db['encouragements']
+    encouragements.append(encouraging_message)
+    db['encouragements'] = encouragements
+  else:
+    db['encouragements'] = [encouraging_message]
+
+
+def delete_encouragements(index):
+  encouragements = db['encouragements']
+  if len(encouragements) > index:
+    del encouragements[index]
+    db['encouragements'] = encouragements
 
 
 @client.event
@@ -40,8 +57,25 @@ async def on_message(message):
     quote = get_quote()
     await message.channel.send(quote)
 
+  options = starter_encouragements
+  if 'encouragements' in db.keys():
+    options = options + list(db['encouragements'])
+
   if any(word in msg for word in sad_words):
-    await message.channel.send(random.choice(starter_encouragements))
+    await message.channel.send(random.choice(options))
+
+  if msg.startswith('$new'):
+    encouraging_message = msg.split('$new ', 1)[1]
+    update_encouragements(encouraging_message)
+    await message.channel.send('New encouraging message added.')
+
+  if msg.startswith('$del'):
+    encouragements = []
+    if 'encouragements' in db.keys():
+      index = int(msg.split('$del', 1)[1])
+      delete_encouragements(index)
+      encouragements = db['encouragements']
+      await message.channel.send(encouragements)
 
 
 client.run(os.getenv('TOKEN'))
